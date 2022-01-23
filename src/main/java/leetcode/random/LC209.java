e12529971b6d4ac8e33b1681e3c493656c6439df
이진우 <nice7677@naver.com>
2022-01-24 02:14:50 +0800

    lc 1859

---
+++ src/main/java/leetcode/random/LC209.java
@@ -0,0 +1,18 @@
+package leetcode.random;
+
+
+/**
+ * LeetCode Random Pcik One
+ * 209. Minimum Size Subarray Sum
+ */
+public class LC209 {
+
+    public static void main(String[] args) {
+        System.out.println(minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        return 0;
+    }
+
+}
