771c4ec651db0517840af7cc4354b19c306ba354
진우 <nice7677@naver.com>
2022-09-19 23:43:23 +0900

    백준 2018

---
+++ src/main/java/baekjoon/BJ2018.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 백준 2018 수들의 합 5
+ */
+public class BJ2018 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+        while (end_index != N) {
+            if (sum == N) {
+                count++;
+                end_index++;
+                sum = sum + end_index;
+            } else if (sum > N) {
+                sum = sum - start_index;
+                start_index++;
+            } else {
+                end_index++;
+                sum = sum + end_index;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
