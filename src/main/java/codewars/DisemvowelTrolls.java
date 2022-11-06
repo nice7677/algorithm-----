a0f65a8f894574c61cd9c366300c8ca20729291c
진우 <nice7677@naver.com>
2022-11-06 12:35:28 +0900

    코드워 모음제거

---
+++ src/main/java/codewars/DisemvowelTrolls.java
@@ -0,0 +1,36 @@
+package codewars;
+
+import java.util.List;
+
+/**
+ * 문장에서 모음 제거하기
+ * 정규표현식이 더 좋다.
+ */
+public class DisemvowelTrolls {
+
+    public static List<String> vowelList = List.of("A", "E", "I", "O", "U");
+
+    public static void main(String[] args) {
+
+        System.out.println(disemvowel("This website is for losers LOL!"));
+
+    }
+
+    public static String disemvowel(String str) {
+        // Code away...
+        String[] strArray = str.split("");
+        StringBuilder sb = new StringBuilder();
+        for (String s : strArray) {
+            if (!isVowel(s)) {
+                sb.append(s);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static boolean isVowel(String word) {
+        return vowelList.stream()
+                .anyMatch(s -> s.equals(word) || s.toLowerCase().equals(word));
+    }
+
+}
