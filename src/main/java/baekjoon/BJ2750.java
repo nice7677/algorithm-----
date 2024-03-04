2d55d8b24c6f8fc4559552e6a287b0ad42f171da
진우 <nice7677@naver.com>
2024-03-04 20:28:43 +0900

    수정렬하기1

--- src/main/java/baekjoon/BJ2750.java
+++ src/main/java/baekjoon/BJ2750.java
@@ -3,8 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 수 정렬하기 백준 2750
@@ -18,14 +16,46 @@ public class BJ2750 {
      */
     public static void main(String[] args) throws IOException {
 
+        bubble();
+
+    }
+
+    public static void bubble() throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            list.add(Integer.valueOf(br.readLine()));
+        int count = Integer.parseInt(br.readLine());
+        int[] number = new int[count];
+        for (int i = 0; i < count; i++) {
+            number[i] = Integer.parseInt(br.readLine());
         }
-        list.stream().sorted()
-                .forEach(System.out::println);
+        while (true) {
+            int swapCount = 0;
+            for (int i = 0; i < number.length; i++) {
+                swapCount = swap(swapCount, number, i, i + 1);
+            }
+            if (swapCount == 0) {
+                break;
+            }
+        }
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+    }
+
+    public static int swap(int count, int[] values, int x, int y) {
+
+        if (x == values.length - 1) {
+            return count;
+        }
+
+        if (values[x] > values[y]) {
+            int value = values[x];
+            values[x] = values[y];
+            values[y] = value;
+            return count + 1;
+        }
+
+        return count;
 
     }
 
